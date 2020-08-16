8e1d227115b345fb006f1502e67a363b92507492
Elisabeth Lardiere <babeth.lardiere@gmail.com>
2020-08-17 00:24:35 +0200

    base de mise en forme css

--- js/index.js
+++ js/index.js
@@ -24,17 +24,19 @@ let teddiesList = document.createElement("ul");
 function displayTeddies(teddies) {
     for(let teddy of teddies) {
 
-// Création des nouveaux éléments du DOM : conteneurs individuels d'oursons avec nom / image / prix
+// Création des nouveaux éléments du DOM : conteneurs individuels d'oursons avec nom/image/lien vers le produit/prix
         let teddyBox= document.createElement("li");
         let teddyName = document.createElement("h2");
         let teddyImage = document.createElement("img");
+        let teddyLink = document.createElement("a");
         let teddyPrice = document.createElement("p");
 
-// Ajout des id et des classes aux nouveaux éléments du DOM
+// Ajout des attributs aux nouveaux éléments du DOM
         teddiesList.classList.add("teddiesList");
         teddyBox.classList.add("teddyBox");
         teddyName.classList.add("teddyName");
         teddyImage.classList.add("teddyImage");
+        teddyLink.classList.add("teddyLink");
         teddyPrice.classList.add("teddyPrice");
 
 // Intégration des nouveaux éléments au DOM
@@ -43,6 +45,7 @@ function displayTeddies(teddies) {
         teddyBox.appendChild(teddyName);
         teddyBox.appendChild(teddyImage);
         teddyBox.appendChild(teddyPrice);
+        teddyImage.appendChild(teddyLink);
 
 // Ajout du contenu texte et image pour chaque ourson
         teddyName.innerHTML = teddy.name;

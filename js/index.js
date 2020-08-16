a72e5cd25e1a5a70371d5018403581fb3baf9415
Elisabeth Lardiere <babeth.lardiere@gmail.com>
2020-08-16 21:02:27 +0200

    affichage de la liste des oursons disponibles

---
+++ js/index.js
@@ -0,0 +1,52 @@
+// Récupération de la liste des oursons auprès de l'API
+fetch('http://localhost:3000/api/teddies')
+    .then(function(response) {
+        if(response.ok) {
+            response.json()
+                .then(function(teddies) {
+                    console.log(teddies);
+                    displayTeddies(teddies);
+                });
+        }
+    })
+    .catch(function() {
+        console.log("erreur");
+        alert("erreur");
+    });
+
+// Récupération des éléments du DOM
+let main = document.querySelector("main");
+
+// Création des nouveaux éléments du DOM : liste
+let teddiesList = document.createElement("ul");
+
+// Affichage de la liste des oursons disponibles
+function displayTeddies(teddies) {
+    for(let teddy of teddies) {
+
+// Création des nouveaux éléments du DOM : conteneurs individuels d'oursons avec nom / image / prix
+        let teddyBox= document.createElement("li");
+        let teddyName = document.createElement("h2");
+        let teddyImage = document.createElement("img");
+        let teddyPrice = document.createElement("p");
+
+// Ajout des id et des classes aux nouveaux éléments du DOM
+        teddiesList.classList.add("teddiesList");
+        teddyBox.classList.add("teddyBox");
+        teddyName.classList.add("teddyName");
+        teddyImage.classList.add("teddyImage");
+        teddyPrice.classList.add("teddyPrice");
+
+// Intégration des nouveaux éléments au DOM
+        main.appendChild(teddiesList);
+        teddiesList.appendChild(teddyBox);
+        teddyBox.appendChild(teddyName);
+        teddyBox.appendChild(teddyImage);
+        teddyBox.appendChild(teddyPrice);
+
+// Ajout du contenu texte et image pour chaque ourson
+        teddyName.innerHTML = teddy.name;
+        teddyImage.setAttribute("src", teddy.imageUrl);
+        teddyPrice.innerHTML = teddy.price;
+    }
+}
